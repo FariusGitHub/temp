c828f3bf7cbf629b38c20a3ccc6a42a27ce08af2
farius tjioesman <ftjioesman@hotmail.com>
2024-03-24 00:53:09 -0400

    add curves

---
+++ code/curve1/curve1/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
+# Initialize the EV3 brick.
+ev3 = EV3Brick()
+# Initialize a motor at port B.
+test_motor = Motor(Port.B)
+# Play a sound.
+ev3.speaker.beep()
+# Run the motor up to certain degrees per second. 
+test_motor.run_target(1000, 360*-1.865)
+# Play another beep sound.
+ev3.speaker.beep(-5.529*100, 100)
